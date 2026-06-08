06d488ee5db454ca261b8b5bafa7035260fc7c4a
Jake Ford <jake.ford@scsstudent.org>
2026-06-07 22:24:11 -0700

    feat: Complete video 8.5

--- JakeProject/Assets/scripts/EnemyMovement.cs
+++ JakeProject/Assets/scripts/EnemyMovement.cs
@@ -8,19 +8,37 @@ public class EnemyMovement : MonoBehaviour
     [field: SerializeField]
     public Waypoint Target { get; private set; }
 
-    void Update()
-{
-    if (Target == null) return;
-
-    transform.position = Vector3.MoveTowards(
-        transform.position,
-        Target.transform.position,
-        Speed * Time.deltaTime
-    );
-
-    if (transform.position == Target.transform.position)
+    void Start()
     {
-        Target = Target.Next; // move to next waypoint
+        if (Target != null)
+        {
+            transform.LookAt(Target.transform);
+        }
     }
-}
+
+    void Update()
+    {
+        if (Target == null) return;
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            Target.transform.position,
+            Speed * Time.deltaTime
+        );
+
+        if (transform.position == Target.transform.position)
+        {
+            SetTarget(Target.Next);
+        }
+    }
+
+    void SetTarget(Waypoint nextWaypoint)
+    {
+        Target = nextWaypoint;
+
+        if (Target != null)
+        {
+            transform.LookAt(Target.transform);
+        }
+    }
 }
